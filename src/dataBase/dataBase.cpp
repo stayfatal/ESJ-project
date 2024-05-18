42442e3c8008764fedde136bed4510a49607848a
stayfatal <staayfatal@gmail.com>
2024-05-18 15:11:37 +0300

    сделал регистрацию "одну на чат" пользователь второй раз не сможет регистрироваться, сделал дочерний класс бд для регистрации

--- src/dataBase/dataBase.cpp
+++ src/dataBase/dataBase.cpp
@@ -1,14 +1,18 @@
 #include "dataBase.h"
 #include <fstream>
+#include <iostream>
 #include <vector>
 #include <iterator>
+#include <stdio.h>
+#include <cstdio>
+
 using std::string;
 using std::vector;
 
-DataBase::DataBase(string fileName){ this->fileName=fileName;}
+DataBase::DataBase(string fileName){ this->fileName=fileName; this->path=folder+fileName; }
 
 void DataBase::editDb(int line,string newLine,bool overwrite){
-    std::ifstream in(fileName);
+    std::ifstream in(path);
     vector<string> vec;
     if(in.is_open()){
         string temp;
@@ -18,22 +22,22 @@ void DataBase::editDb(int line,string newLine,bool overwrite){
         if(overwrite){
             vec[line]=newLine;
         }else{
-            vec[line]+=":"+newLine;
+            vec[line]+=newLine;
         }
         in.close();
     }
 
-    std::ofstream out(fileName);
+    std::ofstream out(path);
     if(out.is_open()){
         for(auto i:vec){
-            out << i;
+            out << i << std::endl;
         }
         out.close();
     }
 }
 
 int DataBase::findLine(string strLine){
-    std::ifstream in(fileName);
+    std::ifstream in(path);
     if(in.is_open()){
         std::string temp;
         int curLine=0;
@@ -47,4 +51,47 @@ int DataBase::findLine(string strLine){
         in.close(); 
     }
     return -1;
+}
+
+RegistrationDataBase::RegistrationDataBase(string fileName) : DataBase(fileName){}
+
+int RegistrationDataBase::findId(int64_t id){
+    std::ifstream in(path);
+    if(in.is_open()){
+        string temp;
+        string strId=std::to_string(id);
+        int curLine=0;
+        while(std::getline(in,temp)){
+            curLine++;
+            size_t iter=0;
+            int counter=0;
+            while(counter<3 and iter!=std::string::npos){
+                iter=temp.find(":",iter+1);
+                counter++;
+            }
+            if(strId==temp.substr(iter+1,temp.length())){
+                in.close();
+                return curLine;
+            }
+        }
+        in.close();
+    }
+    return -1;
+}
+
+bool RegistrationDataBase::registration(string name,int64_t chatId){
+    if(findLine(name)!=-1){
+        editDb(findLine(name)-1,":"+std::to_string(chatId),false);
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool RegistrationDataBase::isRegistered(int64_t chatId){
+    if(findId(chatId)!=-1){
+        return true;
+    }else{
+        return false;
+    }
 }
--- src/dataBase/dataBase.h
+++ src/dataBase/dataBase.h
@@ -7,14 +7,27 @@
 using std::string;
 
 class DataBase{
-private:
+protected:
 std::string fileName;
+std::string path;
+const std::string folder="/home/stayfatal/projects/ESJ-project/data/";
 public:
 DataBase(string fileName);
 
 int findLine(string line);
 
 void editDb(int line,string newLine,bool overwrite=true);
+};
+
+class RegistrationDataBase : private DataBase{
+public:
+RegistrationDataBase(string fileName);
+
+int findId(int64_t id);
+
+bool registration(string name, int64_t chatId);
+
+bool isRegistered(int64_t chatId);
 };
 
 #endif
